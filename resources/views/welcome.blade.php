642d0056947104375192deccae3efd8609cf4dfc
nugiedragon1 <43334123+nugiedragon1@users.noreply.github.com>
2020-04-14 17:21:52 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel is successfuly installed bichis
+                    Suted bau
                 </div>
 
                 <div class="links">
